6b45ce93231691b3a53b4da0b2d2b1010f3eb0a1
loicdescotte <loic.descotte@gmail.com>
2017-02-24 16:04:15 +0100

    simplify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import App from './App';
+import { Grid, Jumbotron } from 'react-bootstrap';
+import routes from './routes.js';
 import './index.css';
 
 ReactDOM.render((
-  <App/>
+  <div>
+    <Jumbotron>
+      <Grid>
+        <h1>GM-CMS</h1>
+        <br/>
+        <div>
+          {routes}
+        </div>
+        <p>
+        </p>
+      </Grid>
+    </Jumbotron>
+  </div>
 ), document.getElementById('root'))
